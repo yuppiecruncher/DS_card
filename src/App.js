2397ac241f66d0e03df7b2d9ba9d36ce3ccd8f20
Ian <yuppiecruncher@gmail.com>
2021-01-27 16:02:00 -0800

    Added GH Actions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import profileImage from './img/ian_profile.jpg'
 
 function App() {
   return (
     <div className="App">
       <h1> My Biz Card </h1>
       <h2> Author: Ian Smith </h2>
+      <p> Web Builder Extraordinaire </p>
+      <ul><li>Now with more ACTIONS!</li></ul>
+      <img className="headshot" src={profileImage} alt="Profile Image" />
     </div>
   );
 }
